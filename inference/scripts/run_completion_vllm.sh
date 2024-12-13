4cd23759891584c119084d4e6f194fe280937167
ytxmobile98 <5900105+ytxmobile98@users.noreply.github.com>
2024-12-13 11:22:11 +0800

    fix: `mkdir -p` output directory before executing

--- inference/scripts/run_completion_vllm.sh
+++ inference/scripts/run_completion_vllm.sh
@@ -1,18 +1,21 @@
 CURDIR=$(dirname "$(realpath "$0")")
+OUTDIR="$CURDIR/completion_result"
 INFERENCE_VLLM="$CURDIR/../inference_vllm.py"
 
 MODEL_DIR='/data/models/Qwen2.5-Coder-7B-Instruct'
+
+mkdir -p "$OUTDIR"
 
 COMPLETE_DATA_PATH="$CURDIR/completion/merge"
 python "$INFERENCE_VLLM" \
     --data_path "$COMPLETE_DATA_PATH" \
     --base_model "$MODEL_DIR" \
     --task 'completion'  \
-    --outdir 'completion_result'
+    --outdir "$OUTDIR"
 
 COMPLETE_DATA_PATH="$CURDIR/completion/light"
 python "$INFERENCE_VLLM" \
     --data_path "$COMPLETE_DATA_PATH" \
     --base_model "$MODEL_DIR" \
     --task 'completion_light'  \
-    --outdir 'completion_result'
+    --outdir "$OUTDIR"
