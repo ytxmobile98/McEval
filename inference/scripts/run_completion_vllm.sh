09275bf3fdd856843abfe31a813c38fe2f4a526b
ytxmobile98 <5900105+ytxmobile98@users.noreply.github.com>
2024-12-13 13:32:19 +0800

    chore: move completion directory to upper level

--- inference/scripts/run_completion_vllm.sh
+++ inference/scripts/run_completion_vllm.sh
@@ -1,19 +1,19 @@
 CURDIR=$(dirname "$(realpath "$0")")
-OUTDIR="$CURDIR/completion_result"
+OUTDIR="$CURDIR/../completion_result"
 INFERENCE_VLLM="$CURDIR/../inference_vllm.py"
 
 MODEL_DIR='/data/models/Qwen2.5-Coder-7B-Instruct'
 
 mkdir -p "$OUTDIR"
 
-COMPLETE_DATA_PATH="$CURDIR/completion/merge"
+COMPLETE_DATA_PATH="$CURDIR/../completion/merge"
 python "$INFERENCE_VLLM" \
     --data_path "$COMPLETE_DATA_PATH" \
     --base_model "$MODEL_DIR" \
     --task 'completion'  \
     --outdir "$OUTDIR"
 
-COMPLETE_DATA_PATH="$CURDIR/completion/light"
+COMPLETE_DATA_PATH="$CURDIR/../completion/light"
 python "$INFERENCE_VLLM" \
     --data_path "$COMPLETE_DATA_PATH" \
     --base_model "$MODEL_DIR" \
