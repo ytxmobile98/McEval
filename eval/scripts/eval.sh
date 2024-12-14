467c7a40df5c3df7f26641c114ad404c4a904d33
ytxmobile98 <5900105+ytxmobile98@users.noreply.github.com>
2024-12-14 11:44:08 +0800

    feat: use `for` loop for eval.sh

--- eval/scripts/eval.sh
+++ eval/scripts/eval.sh
@@ -1,15 +1,12 @@
 CURDIR=$(dirname "$(realpath "$0")")
 source "$CURDIR/../../common.env.sh"
 
-# TASK=completion_light
-TASK=completion
-# TASK=explain_stage1
-# TASK=explain_stage2
-# TASK=generation
-
 RESULTS_DIR=$(realpath "$CURDIR/../../results")
-RESULT_PATH="$RESULTS_DIR/eval/split/${MODEL}_${TASK}"
-SAVE_PATH="$RESULTS_DIR/eval/outputs/${MODEL}_${TASK}"
 
-mkdir -p "$SAVE_PATH"
-python -u "$CURDIR/../eval_all.py" --result_path "$RESULT_PATH" --save_path "$SAVE_PATH"
+for TASK in completion_light completion explain_stage1 explain_stage2; do
+    RESULT_PATH="$RESULTS_DIR/eval/split/${MODEL}_${TASK}"
+    SAVE_PATH="$RESULTS_DIR/eval/outputs/${MODEL}_${TASK}"
+
+    mkdir -p "$SAVE_PATH"
+    python -u "$CURDIR/../eval_all.py" --result_path "$RESULT_PATH" --save_path "$SAVE_PATH"
+done
